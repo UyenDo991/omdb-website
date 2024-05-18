cf215e7728232550cea74f45e9ef595761e9ac39
Nguyễn Ngọc Quang Bảo <bao.nnq1603@gmail.com>
2024-05-18 15:39:25 +0700

    Bao commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,42 +18,14 @@ onMounted(async () => {
   console.log("tvGenresList:", tvGenresList) */
   const res = await getNowPlayling();
   if (res) {
-    nowPlayingList.value = [...res.results]
+    // const chunk = chunkArray(res.results, 4);
+    nowPlayingList.value = chunkArray(res.results, 4);
   }
-  // console.log(nowPlayingList.value);
-  const chunk = chunkArray(nowPlayingList.value, 4);
-  console.log(chunk)
+  console.log(nowPlayingList.value);
 })
 </script>
 
 <template>
-  <v-container fluid>
-    <v-row dense align="center" justify="space-between">
-      <v-col cols="4" v-for="movie in nowPlayingList" :key="movie.id">
-        <!-- <v-card outlined :height="250">
-          <v-img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`" height="50%"></v-img>
-          <v-card-title>{{movie.title}}</v-card-title>
-          <v-card-text>{{movie.overview}}</v-card-text>
-        </v-card> -->
-        <v-card color="#1F7087" class="mx-2">
-          <div class="d-flex flex-no-wrap justify-space-between" @click="viewDetail(movie.id)">
-            <v-sheet color="transparent" width="50%">
-              <v-card-title class="text-h5">
-                {{ movie.title }}
-              </v-card-title>
-              <v-card-subtitle>{{ movie.overview }}</v-card-subtitle>
-              <v-card-actions>
-                <v-btn class="ms-2" size="small" text="View" variant="outlined"></v-btn>
-              </v-card-actions>
-            </v-sheet>
-            <v-avatar class="ma-3" rounded="0" size="125">
-              <v-img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`"></v-img>
-            </v-avatar>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
   <section id="center" class="center_home">
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
@@ -172,10 +144,7 @@ onMounted(async () => {
       <div class="row trend_2 mt-4">
         <div id="carouselExampleCaptions1" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide-to="0" class="active"
-              aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide-to="1" aria-label="Slide 2"
-              class="" aria-current="true"></button>
+            <button v-for="(index) in nowPlayingList" :key="index" type="button" data-bs-target="#carouselExampleCaptions1" :data-bs-slide-to="index" :class="index === 0 ? 'active' : ''" :aria-label="`Slide ${index}`"></button>
           </div>
           <div class="carousel-inner">
             <div class="carousel-item active">
