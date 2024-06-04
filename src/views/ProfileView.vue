dc19861bea521085a22052911a2681126ab9bc2c
Thao uyen do <thaonguyen1571998@gmail.com>
2024-06-04 08:48:04 +0700

    upd

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -153,13 +153,3 @@ function getRandomIntInclusive(min, max) {
     </div>
   </section>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
